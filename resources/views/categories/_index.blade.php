0d755631e25e025d86cdc9de9167993ff4d05710
Pierre <schererpierre.pro@gmail.com>
2021-10-05 14:31:22 +0200

    Intégration des recents blogs + Tags Cloud

--- resources/views/categories/_index.blade.php
+++ resources/views/categories/_index.blade.php
@@ -9,4 +9,4 @@
         </li>
       @endforeach
     </div>
-  </div>
+</div>
---
+++ resources/views/tags/_tagcloud.blade.php
@@ -0,0 +1,6 @@
+<div class="sidebar-box ftco-animate">
+    <div class="tag-cloud">
+        <h3>Tag Cloud</h3>
+        @include('tags._list', ['tags' => $tags])
+    </div>
+</div>
